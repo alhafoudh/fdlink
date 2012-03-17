96cf8ad926bc6b367c89f0c8aa8f582496999c38
Benito van der Zander <benito@benibela.de>
2012-03-17 18:11:57 +0100

    fix on new kernel >= aae8a97d3ec30788790d1720b71d76fd8eb44b73

--- flink/flink_dev.c
+++ flink/flink_dev.c
@@ -18,12 +18,64 @@
 
 #include "flink.h"
 
+#include "linux/fsnotify.h"
+
+//copied from kernel namei.c
+static inline int may_create(struct inode *dir, struct dentry *child)
+{
+	if (child->d_inode)
+		return -EEXIST;
+	if (IS_DEADDIR(dir))
+		return -ENOENT;
+	return inode_permission(dir, MAY_WRITE | MAY_EXEC);
+}
+
+//copied from kernel namei.c revision 83e92ba, removed security call
+static int flink_vfs_link_83e92ba(struct dentry *old_dentry, struct inode *dir, struct dentry *new_dentry)
+{
+	struct inode *inode = old_dentry->d_inode;
+	int error;
+
+	if (!inode)
+		return -ENOENT;
+
+	error = may_create(dir, new_dentry);
+	if (error)
+		return error;
+
+	if (dir->i_sb != inode->i_sb)
+		return -EXDEV;
+
+	/*
+	 * A link to an append-only or immutable file cannot be created.
+	 */
+	if (IS_APPEND(inode) || IS_IMMUTABLE(inode))
+		return -EPERM;
+	if (!dir->i_op->link)
+		return -EPERM;
+	if (S_ISDIR(inode->i_mode))
+		return -EPERM;
+
+	//-------------------Removed security_inode_link call because it is not exported 
+	//error = security_inode_link(old_dentry, dir, new_dentry);
+	//if (error)
+	//	return error;
+
+	mutex_lock(&inode->i_mutex);
+	error = dir->i_op->link(old_dentry, dir, new_dentry);
+	mutex_unlock(&inode->i_mutex);
+	if (!error)
+		fsnotify_link(dir, inode, new_dentry);
+	return error;
+}
+
+
 static long flink_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	struct flink __user *p = (struct flink __user *)arg;
 	struct dentry *old_dentry;
 	struct file *f;
-	struct nameidata nd;
+	struct path new_path;
 	struct dentry *new_dentry;
 
 	char *to = getname(p->path);
@@ -41,24 +93,26 @@ static long flink_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	old_dentry = f->f_dentry;
 	error = 0;
 
-	error = path_lookup(to, LOOKUP_PARENT, &nd);
-	if (error)
+	error = kern_path(to, LOOKUP_PARENT, &new_path);
+	if (error) {
 		goto release_f;
+	}
 
 	error = -EXDEV;
-	if (f->f_vfsmnt != nd.path.mnt)
+	if (f->f_vfsmnt != new_path.mnt)
 		goto release_nd;
 
-	new_dentry = lookup_create(&nd, 0);
+        struct path unused;
+	new_dentry = kern_path_create(AT_FDCWD, to, &unused, 0);
 	error = PTR_ERR(new_dentry);
 	if (!IS_ERR(new_dentry)) {
-		error = vfs_link(old_dentry, nd.path.dentry->d_inode, new_dentry);
+		error = flink_vfs_link_83e92ba(old_dentry, new_path.dentry->d_inode, new_dentry);
 		dput(new_dentry);
 	}
-	mutex_unlock(&nd.path.dentry->d_inode->i_mutex);
+	mutex_unlock(&new_path.dentry->d_inode->i_mutex);
 
 release_nd:
-	path_put(&nd.path);
+	path_put(&new_path);
 release_f:
 	fput(f);
 exit:
@@ -103,3 +157,4 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Amos Shapira <amos.shapira@gmail.com>");
 MODULE_DESCRIPTION("flink module");
 MODULE_VERSION("dev");
+
